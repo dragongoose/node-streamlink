467a0b4397771c26b06e2cf6c0ad3b8e1f228835
Filip Stenbacka <filip.stenbacka@gmail.com>
2020-03-15 23:43:02 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# STREAMLINK
+Streamlink node wrapper. Modified from [https://github.com/sanchezand/node-streamlink].
+
+
+## Installation
+```bash
+npm install streamlink
+```
--- src/index.ts
+++ src/index.ts
@@ -1,149 +1,126 @@
-import dgram from "dgram";
+import { ChildProcessWithoutNullStreams, exec, spawn } from "child_process";
 import { EventEmitter } from "events";
+import fs from "fs";
+import kill from "tree-kill";
 
-export interface IProduct {
-  amount: number;
-  name: string;
+export interface IEnd {
+  exitCode?: number | string;
+  duration?: number | null;
+  output?: string;
+  stream: string;
 }
 
-export interface ISlot {
-  slot: number;
-  amount: number;
-  name: string;
-  status?: VulvaStatus;
-}
-
-type VulvaStatus = "EMPTY" | "DISABLED" | "POSITIONERROR";
-
-export declare interface VulvaPoller {
-  on(event: 'connect'): this;
-  on(event: 'close'): this;
-  on(event: 'listening'): this;
-  on(event: 'message', listening: (msg: string) => void): this;
+export declare interface Streamlink {
+  on(event: 'begin', listening: (link: string) => void): this;
+  on(event: 'end', listening: (info: IEnd) => void): this;
+  on(event: 'close', listening: () => void): this;
   on(event: 'error', listening: (error: Error) => void): this;
-  on(event: 'status', listening: (slots: ISlot[]) => void): this;
-  on(event: 'added', listening: (products: IProduct[]) => void): this;
-  on(event: 'removed', listening: (products: IProduct[]) => void): this;
+  on(event: 'log', listening: (log: string) => void): this;
 }
 
-export class VulvaPoller extends EventEmitter {
-  private msg: string;
-  private socket: dgram.Socket;
-  private port: number;
-  private address: string | undefined;
-  private slots: ISlot[] = [];
-  private open: boolean = false;
+export class Streamlink extends EventEmitter {
+  public stream: string;
+  public streaming: boolean = false;
+  private outputLocation?: string;
+  private qual: string = "best";
+  private child?: ChildProcessWithoutNullStreams;
+  private startTime?: number;
+  private qualities: string[] = [];
 
-  public constructor(msg: string, port: number, address?: string) {
+  constructor(stream: string) {
     super();
-    this.msg = msg;
-    this.port = port;
-    this.address = address;
-    this.socket = dgram.createSocket("udp4");
-
-    this.socket.on("connect", () => {
-      this.open = true;
-      this.emit("connect");
-    });
-    this.socket.on("close", () => {
-      this.open = false;
-      this.emit("close");
-    });
-    this.socket.on("listening", () => this.emit("listening"));
-    this.socket.on('error', (err) => {
-      this.emit("error", err);
-      this.socket.close();
-    });
-
-    // The only message should be the current status
-    this.socket.on('message', msg => {
-      const a: ISlot[] = msg.toString().split("\n").slice(0, -1).map((s, i) => ({
-          slot: i,
-          name: s.slice(6).split("(").reverse()[1].trim(),
-          amount: Number(s.split("(").reverse()[0].split(")")[0]),
-          status: s.split("\t")[1] as VulvaStatus,
-      }));
-
-      this.emit("status", a);
-      this.handleNewStatus(a);
-    });
-
-    this.socket.connect(this.port, this.address);
+    this.stream = stream;
   }
 
-  private handleNewStatus = (slots: ISlot[]) => {
-    if (slots.length !== 8) {
-      this.emit("error", new Error(`Vulva status has length ${slots.length} !== 8.`));
-      return;
+  public output = (location: string) => {
+    this.outputLocation = location;
+    return this;
+  };
+
+  public quality = (quality: string) => {
+    this.qual = quality;
+    return this;
+  }
+
+  public isLive = (callback: (isLive: boolean) => void) => {
+    exec("streamlink -j " + this.stream, (_err, stdout, _stderr) => {
+      const json = JSON.parse(stdout);
+      if (!json.error) {
+        this.qualities = Object.keys(json.streams);
+        callback(true);
+      } else {
+        callback(false);
+      }
+    });
+  }
+
+  public begin = () => {
+    if (this.outputLocation && fs.existsSync(this.outputLocation)) {
+      this.emit("error", "File already exists.");
+      return this;
     }
 
-    if (this.slots.length === 0) {
-      this.slots = slots;
-      this.emit("message", "Starting point set");
-      return;
-    }
-
-    const added: IProduct[] = [];
-    const removed: IProduct[] = [];
-
-    slots.forEach((slot, i) => {
-      if (slot.name !== this.slots[i].name) {
-        if (slot.amount > 0) {
-          added.push({
-            name: slot.name,
-            amount: slot.amount,
-          });
-        }
-
-        if (this.slots[i].amount > 0) {
-          removed.push({
-            name: this.slots[i].name,
-            amount: this.slots[i].amount,
-          });
-        }
+    this.isLive(live => {
+      if (!live) {
+        this.emit("error", "Is not live.");
         return;
       }
 
-      const change = slot.amount - this.slots[i].amount;
-      if (change > 0) {
-        added.push({
-          name: slot.name,
-          amount: change,
-        });
-      } else if (change < 0) {
-        removed.push({
-          name: slot.name,
-          amount: -change,
-        });
+      const args = [];
+      if (this.outputLocation) {
+        args.push("-o");
+        args.push(this.outputLocation);
       }
+      args.push(this.stream);
+      args.push(this.qual);
+      this.startTime = Date.now();
+
+      this.child = spawn("streamlink", args);
+      this.child.stdout.on("data", d => {
+        this.emit("log", d.toString());
+      });
+
+      this.child.on("close", (code, _st) => {
+        this.emit("close");
+        this.end(code);
+      });
+
+      this.streaming = true;
+      this.emit("begin", this.stream);
     });
-
-    if (added.length > 0) {
-      this.emit("added", added);
-    }
-    if (removed.length > 0) {
-      this.emit("removed", removed);
-    }
-
-    this.slots = slots;
+    return this;
   }
 
-  public startPolling = (interval: number, times?: number) => {
-    if (Number(times) <= 0) {
-      return;
-    }
-    setTimeout(() => {
-      if (this.open) {
-        this.emit("message", `Polling${times ? ` (${times - 1} times left)` : ""}.`);
-        this.socket.send(this.msg);
-        this.startPolling(interval, times === undefined ? undefined : times - 1);
-      } else {
-        this.emit("error", "Cannot poll, becasue the socket is closed.");
+  public end = (exitCode: number) => {
+    if (this.streaming) {
+      const res: IEnd = {
+        exitCode,
+        output: this.outputLocation,
+        stream: this.stream,
+      };
+
+      if (this.startTime) {
+        res.duration = Math.floor((Date.now() - this.startTime) / 1000);
       }
-    }, interval);
+
+      this.streaming = false;
+      this.emit("end", res);
+    }
+
+    if (this.child) { 
+      kill(this.child.pid);
+    }
+    return;
   }
 
-  public close = () => {
-    this.socket.close();
+  public getQualities = (): void => {
+    this.isLive(live => {
+      if (!live) {
+        this.emit("error", "Can not get qualities, stream is not live.");
+        return;
+      }
+
+      this.emit("quality", this.qualities);
+    });
   }
 }
